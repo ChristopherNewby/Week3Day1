9017c3b6a6f50747d6799979476d3a0b78b0d353
Christopher Newby <Christopher Newby@DESKTOP-3F7RENE>
2016-10-10 13:50:26 -0400

    Homework for week3day1

---
+++ ConsoleApplication1/ConsoleApplication1/Class2.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public static class WordCountClass
+    {
+        public static int CountVowels(this string originalString)
+        {
+            int count = 0;
+
+            foreach (char c in originalString)
+            {
+                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+                    count++;
+            }
+            count++;
+
+            return count;
+        }
+    }
+
+    public static class Multiply
+    {
+        public static int MultiplyByFive(this int number)
+        {
+            return number * 5;
+        }
+    }
+
+    public static class TwoPointFiveTimes
+    {
+        public static double MultiplyByTwoPointFive(this double number)
+        {
+            return number * 2.5;
+        }
+    }
+
+
+    public static class CheckForA
+    {
+        public static string ACheck(this char nextChar)
+        {
+            if (nextChar == 'a')
+            {
+                return "This is an a";
+            }
+            else
+            {
+                return "This is not an a";
+            }
+        }
+    }
+
+    public static class BoolExtension
+    {
+        public static int returnTwo(this bool var)
+        {
+            if (var == true)
+            {
+                return 2;
+            }
+            else
+            {
+                return 3;
+            }
+        }
+    }
+
+    public static class FloatExtension
+    {
+        public static double multiply(this float number)
+        {
+            Console.WriteLine("Enter the number to be multiplied");
+            string a = Console.ReadLine();
+
+            return number * (Convert.ToDouble(a));
+            
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+   
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string myString = "The quick brown fox jumped over the lazy dog";
+ 
+            int wvowels = myString.CountVowels();
+            Console.WriteLine(wvowels);
+
+            int number = 5.MultiplyByFive();
+            Console.WriteLine(number);
+
+            double newNumber = (1001.05).MultiplyByTwoPointFive();
+            Console.WriteLine(newNumber);
+
+            char aletter = 'a';
+            Console.WriteLine(aletter.ACheck());
+
+            bool check = true;
+            Console.WriteLine(check.returnTwo());
+
+            float anotherNumber = 10.5F;
+            Console.WriteLine(anotherNumber.multiply());
+
+            Console.ReadLine();
+        }
+    }
+}
